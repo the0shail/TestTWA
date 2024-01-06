f29bbeff3462326e3dc61da40481d553f30f7a28
the0shail <nespasibo179@gmail.com>
2024-01-06 13:05:27 +0500

    update

--- registrated.js
+++ registrated.js
@@ -3,6 +3,6 @@ import { parse } from "./util/func.js";
 const query = window.location.search;
 let params = parse(query)
 
-// const
-
+const content = document.getElementById("content")
+content.textContent = `Hello ${params.username}`
 // .innerText = `Hello ${params.username}!`
