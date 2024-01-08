96dba365c8ecbbdcf3fa92486078ec414e83febd
the0shail <nespasibo179@gmail.com>
2024-01-08 15:21:14 +0500

    update

--- util/script.js
+++ util/script.js
@@ -13,7 +13,7 @@ let buttons = document.querySelectorAll(".btn");
 let bucket = []
 
 
-document.getElementById("username").innerText = `${WebApp?.initDataUnsafe?.user?.first_name ?? "Гость"}`;
+document.getElementById("username").innerText = `${WebApp?.initDataUnsafe?.user?.first_name == undefined ? "Гость" : WebApp?.initDataUnsafe?.user?.first_name}`;
 
 buttons.forEach((button, j) => {
     let itemNumber = button.getAttribute("data-list-number");
