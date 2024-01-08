6677e435f0c7c30cabc7354c33790853ac5fe2e3
the0shail <nespasibo179@gmail.com>
2024-01-08 12:35:38 +0500

    update

--- util/script.js
+++ util/script.js
@@ -13,14 +13,18 @@ let buttons = document.querySelectorAll(".btn");
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
-        if (WebApp.MainButton.isVisible) WebApp.MainButton.hide();
-        else {
-            // console.log(item.dataset.listNumber)
-            let list_number = 1;
-            WebApp.MainButton.setText(`Вы выбрали товар ${list_number}`);
-            item = list_number;
-            WebApp.MainButton.show();
-        }
+
+
+
+
+        // if (WebApp.MainButton.isVisible) WebApp.MainButton.hide();
+        // else {
+        //     // console.log(item.dataset.listNumber)
+        //     let list_number = 1;
+        //     WebApp.MainButton.setText(`Вы выбрали товар ${list_number}`);
+        //     item = list_number;
+        //     WebApp.MainButton.show();
+        // }
     })
 })
 
@@ -35,6 +39,12 @@ p.innerText = `${WebApp.initDataUnsafe.user.first_name}`;
 
 usercard.appendChild(p);
 
+
+
+function createButton(){
+    let buttonElement = document.createElement("button");
+    // buttonElement.
+}
 
 
 
