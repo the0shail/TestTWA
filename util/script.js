dfbee85379f55a7dbd3cbd1908d9d354a77376c0
the0shail <nespasibo179@gmail.com>
2024-01-08 13:32:05 +0500

    update

--- util/script.js
+++ util/script.js
@@ -8,23 +8,55 @@ WebApp.expand()
 WebApp.MainButton.textColor = "#fff"
 WebApp.MainButton.color = "#2cab37"
 
-let item = "";
+
 let buttons = document.querySelectorAll(".btn");
 
+let bucket = {}
+
 buttons.forEach(button => {
+    let itemNumber = button.getAttribute("data-list-number");
+
+    console.log(itemNumber)
+
     button.addEventListener("click", () => {
+        button.setAttribute("open", "");
+
+        let {plus, minus, counter, i} = createButton(button);
+
+        plus.addEventListener("click", () => {
+            i++;
+            counter.textContent = i;
+
+            bucket[itemNumber] = i;
+
+            console.log(bucket)
+        })
 
 
+        minus.addEventListener("click", () => {
+            i--;
+            counter.textContent = i;
+            bucket[itemNumber] = i;
+
+            if (i <= 0) {
+                minus.remove();
+                plus.remove();
+                counter.remove();
+                button.textContent = "Add";
+                button.removeAttribute("open");
+                delete bucket[itemNumber];
+            }
+            console.log(bucket)
+        })
 
 
-        // if (WebApp.MainButton.isVisible) WebApp.MainButton.hide();
-        // else {
-        //     // console.log(item.dataset.listNumber)
-        //     let list_number = 1;
-        //     WebApp.MainButton.setText(`Вы выбрали товар ${list_number}`);
-        //     item = list_number;
-        //     WebApp.MainButton.show();
-        // }
+        if (bucket == {}) WebApp.MainButton.hide();
+        else {
+            WebApp.MainButton.setText(`Вы выбрали товаров ${bucket.length}`);
+            WebApp.MainButton.show();
+        }
+
+
     })
 })
 
@@ -40,11 +72,37 @@ p.innerText = `${WebApp.initDataUnsafe.user.first_name}`;
 usercard.appendChild(p);
 
 
+function createButton(elem) {
+    let i = 1;
+    elem.textContent = ''
+    let divElement = document.createElement("div");
+    divElement.classList.add("flex", "justify-center");
+    let plus = document.createElement("button");
+    let minus = document.createElement("button");
+    minus.classList.add("text-white", "uppercase", "text-xl", "font-bold", "rounded-lg", "bg-red-500", "text-center", "w-[40px]", "h-[40px]", "active:bg-red-600", "mr-1");
+    plus.classList.add("text-white", "uppercase", "text-xl", "font-bold", "rounded-lg", "bg-green-500", "text-center", "w-[40px]", "h-[40px]", "active:bg-green-600");
+    minus.textContent = "-"
+    plus.textContent = "+"
 
-function createButton(){
-    let buttonElement = document.createElement("button");
-    // buttonElement.
+    divElement.appendChild(minus);
+    divElement.appendChild(plus);
+
+    let counter = document.createElement('span');
+    counter.classList.add("top-0", "right-2", "lowercase", "bg-orange-500", "rounded-full", "flex", "justify-center", "text-lg", "text-white", "items-center", "font-bold", "absolute", "h-[30px]", "w-[30px]")
+    counter.textContent = 1
+
+    let parent = elem.parentNode;
+
+    parent.appendChild(divElement)
+    parent.appendChild(counter)
+
+    return {plus, minus, counter, i}
 }
+
+// <div className="flex justify-center">
+//     <button class="text-white uppercase text-xl font-bold rounded-lg bg-red-500 text-center w-[40px] h-[40px] active:bg-red-600 mr-1">-</button>
+//     <button class="text-white uppercase text-xl font-bold rounded-lg bg-green-500 text-center w-[40px] h-[40px] active:bg-green-600">+</button>
+// </div>
 
 
 
