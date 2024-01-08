fd22e75173cf81cbdf01e32b7a4bf51015c10178
the0shail <nespasibo179@gmail.com>
2024-01-08 10:46:21 +0500

    update

--- util/script.js
+++ util/script.js
@@ -3,7 +3,36 @@
 
 const WebApp = window.Telegram.WebApp;
 
-console.log(WebApp.showAlert(`Добро пожаловать. @${WebApp.WebAppUser.username}`))
+WebApp.expand()
+
+WebApp.MainButton.textColor = "#fff"
+WebApp.MainButton.color = "#2cab37"
+
+let item = "";
+let buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (WebApp.MainButton.isVisible) WebApp.MainButton.hide();
+        else {
+            let list_number = item.getAttribute("data-list-number");
+            WebApp.MainButton.setText(`Вы выбрали товар ${list_number}`);
+            item = list_number;
+            WebApp.MainButton.show();
+        }
+    })
+})
+
+Telegram.WebApp.onEvent("mainButtonClicked", () => {
+    WebApp.sendData("Hello world")
+})
+
+let usercard = document.getElementById("usercard");
+
+let p = document.createElement("p");
+p.innerText = `${WebApp.initDataUnsafe.first_name}`;
+
+usercard.appendChild(p);
 
 
 
